3439db76ce4fe11ce1a44f31447ea1ece3a3638c
Mahdi Mostafavi <mmostafavi247@gamil.com>
2022-05-14 21:38:45 +0430

    Config nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
